e01ef1a589c78bb3ce3de1d986b1904a48eb01fb
Erisvam <erisvamh@gmail.com>
2021-05-16 12:53:11 -0300

    aplicando menu lateral

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -11,11 +11,21 @@ export class HeaderComponent implements OnInit {
 
   arrStars: Stars[] = null;
   moves: number = 0;
+  show: boolean = true;
 
   constructor(private memoryGameService: MemoryGameService) {}
 
   ngOnInit(): void {
     this.memoryGameService.changeMoves.subscribe(move => this.moves = move);
     this.arrStars = this.memoryGameService.arrStars;
+  }
+
+  menuToggle(){
+    let menuSection = document.querySelector(".menu-section");
+
+    document.body.style.overflow = this.show ? "hidden" : "initial";
+    menuSection.classList.toggle("on", this.show);
+    this.show = !this.show
+
   }
 }
